ced0290bfc9cd1509272dde560f5f634c1c04817
tadikondaharshitha14-ops <tadikonda.harshitha14@gmail.com>
2026-09-23 22:40:31 +0530

    Add files via upload

---
+++ workbookPAC1/Workbook Pac1.py
@@ -0,0 +1,50 @@
+
+
+def merge_sort(arr):
+
+    # Base condition
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+
+    left = arr[:mid]
+    right = arr[mid:]
+
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
+
+
+def merge(left, right):
+
+    result = []
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+  
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
+
+
+
+arr = [38, 12, 27, 43, 9, 31, 18, 25]
+
+print("Original array:", arr)
+
+sorted_arr = merge_sort(arr)
+
+print("Sorted array:", sorted_arr)
